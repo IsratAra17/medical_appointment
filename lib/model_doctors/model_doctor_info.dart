d9f0ff4c6661660f1f0a25126368a4733736c970
s.m.israt <smisratara25@gmail.com>
2023-04-03 11:38:11 +0600

    dependency and model class added

---
+++ lib/model_doctors/model_doctor_info.dart
@@ -0,0 +1,83 @@
+class DrInfoModel {
+  String? name;
+
+  String? image;
+  String? designation;
+  double? rating;
+
+  DrInfoModel({ this.name, this.image, this.designation,this.rating});
+}
+  List<DrInfoModel> drList = [
+    DrInfoModel(
+
+      name: " Dr. S. M. Israt Ara",
+      image:
+"https://thumbs.dreamstime.com/z/muslim-female-doctor-avatar-wearing-hijab-stethoscope-clipart-icon-vector-flat-design-islamic-female-doctor-avatar-animated-241773226.jpg",      designation: "Pediatrician",
+   rating:5.0
+    ),
+    DrInfoModel(
+
+      name: "Dr. Nowrin",
+      image:
+"https://cdn.pixabay.com/photo/2017/01/31/22/32/doctor-2027768_640.png",
+        rating:5.0
+
+    ),
+    DrInfoModel(
+
+      name: "Dr Charlie Black",
+      image:
+
+      "https://cdn.pixabay.com/photo/2017/01/31/22/32/doctor-2027768_640.png",
+      designation: "Cardiologist",
+        rating:4.0
+
+    ),
+    DrInfoModel(
+      name: "Darrel Steward",
+      image: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZG9jdG9yfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      designation: "General Practitioner",
+        rating:4.9
+
+    ),
+    DrInfoModel(
+      name: "Wade Warren",
+      image:"https://images.theconversation.com/files/304957/original/file-20191203-66986-im7o5.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
+      designation: "General Practitioner",
+        rating:5.0
+
+    ),
+    DrInfoModel(
+
+      name: "Dr Chris Frazier",
+      image:
+          "https://cdn.pixabay.com/photo/2017/01/31/22/32/doctor-2027768_640.png",
+      designation: "Pediatrician",
+        rating:4.9
+
+    ),
+
+
+    DrInfoModel(
+
+      name: "Dr Charlie Black",
+      image:
+      "https://cdn.pixabay.com/photo/2017/01/31/22/32/doctor-2027768_640.png",
+      designation: "Cardiologist",
+      rating: 4.9
+    ),
+    DrInfoModel(
+        name: "Darrel Steward",
+        image: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZG9jdG9yfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+        designation: "General Practitioner",
+      rating: 4.8
+    ),
+    DrInfoModel(
+        name: "Wade Warren",
+        image:"https://images.theconversation.com/files/304957/original/file-20191203-66986-im7o5.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop",
+        designation: "General Practitioner",
+      rating: 4.7
+    ),
+
+  ];
+
